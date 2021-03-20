92bf243e808a801589af66f04b657265aee4fbc1
Java02 <alekseenkooleg2004@gmail.com>
2021-03-20 10:48:59 +0200

    Initial commit

---
+++ src/pasha/edu/DoorType.java
@@ -0,0 +1,10 @@
+package pasha.edu;
+
+/*
+@USER: Java02
+@DATE: 20.03.2021
+@NAME: DoorType
+*/
+public enum DoorType {
+    INTERNAL , EXTERNAL
+}
---
+++ src/pasha/edu/Main.java
@@ -0,0 +1,18 @@
+package pasha.edu;
+
+public class Main {
+
+    public static void main(String[] args) {
+Door belarusDoor = new Door(
+        1,
+        2,
+        "wood",
+        "white",
+        "modern",
+        true ,
+        true ,
+        true ,
+        DoorType.EXTERNAL);
+
+    }
+}
